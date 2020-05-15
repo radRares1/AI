ac055f3cfc357311b3dd364496f596fd0322e3c2
radRares1 <rad.rares1@gmail.com>
2020-05-15 16:28:06 +0300

    lab9 again

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -68,7 +68,7 @@ model.add(Dropout(0.2))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
-# compiling the sequential model
+# compiling the model
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 
 # training the model and saving metrics
